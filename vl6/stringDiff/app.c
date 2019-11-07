0324ded9f720a60020f9177b3cb458189d4bac4c
Daniel Ullrich <daniel.ullrich@student.fhws.de>
2019-11-08 00:44:28 +0100

    adding

--- vl6/stringDiff/app.c
+++ vl6/stringDiff/app.c
@@ -3,5 +3,15 @@
 
 int main(void)
 {
+	char text1[255];
+	printf("text1: ");
+	scanf("%s", text1);
+	
+	char text2[255];
+	printf("text2: ");
+	scanf("%s", text2);
+
+	printf("Diff: %i \n", countDiffs(text1, text2));
+	
 	return 0;
 }
