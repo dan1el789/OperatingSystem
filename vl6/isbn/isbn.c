40a1e10f94ce4b4278623fbf585d3e4221a9205e
Daniel Ullrich <daniel.ullrich@student.fhws.de>
2019-11-07 21:43:01 +0100

    add length function

--- vl6/isbn/isbn.c
+++ vl6/isbn/isbn.c
@@ -2,3 +2,10 @@ int checkIsbn(char* c)
 {
 	return -1;
 }
+
+int checkLength(char* c)
+{
+	int length;
+	for(length = 0; c[length] != '\0'; length++);
+	return length - 9;
+}
--- vl6/isbn/isbn.h
+++ vl6/isbn/isbn.h
@@ -1,1 +1,3 @@
 int checkIsbn(char* c);
+int checkLength(char* c);
+#include "isbn.c"
