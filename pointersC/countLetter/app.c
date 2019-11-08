2a44f2d7ba5e15eaf335147cf87d8873ad05db72
Daniel Ullrich <daniel.ullrich@student.fhws.de>
2019-11-09 00:01:24 +0100

    add function for counting char

--- pointersC/countLetter/app.c
+++ pointersC/countLetter/app.c
@@ -3,5 +3,14 @@
 
 int main(void)
 {
+	char text[255];
+	printf("word: ");
+	scanf("%s", text);
+	
+	char letter;
+	printf("letter: ");
+	scanf(" %c", &letter);
+
+	printf("%i \n", countLetter(text, letter));
 	return 0;
 }
--- pointersC/countLetter/countLetter.c
+++ pointersC/countLetter/countLetter.c
@@ -1,1 +1,12 @@
 #include "countLetter.h"
+
+int countLetter(char* text, char letter)
+{
+	int counter = 0;
+	for(int i = 0 ; text[i] != '\0'; i++)
+	{
+		if(text[i] == letter)
+			counter++;
+	}
+	return counter;
+}
