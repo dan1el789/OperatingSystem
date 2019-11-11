ae7086acc6da88606ab21b3ca900b043a258bdb9
Daniel Ullrich <daniel.ullrich@student.fhws.de>
2019-11-11 22:31:31 +0100

    add commands

--- unixAndBash/excercisesInScript.sh
+++ unixAndBash/excercisesInScript.sh
@@ -5,4 +5,12 @@ echo $PS1
 export PS1="\u>"
 #bash custom prompt https://www.cyberciti.biz/tips/howto-linux-unix-bash-shell-setup-prompt.html
 
+#How can you list subdirectories with ls?
+ls -R 
+
+#How can you determine the available space on the drive in MegaByte
+df --block-size=1024 --total 
+
+#determine the number of users currently logged in
+who -u
 
---
+++ unixAndBash/importantCommands/README.md
@@ -0,0 +1,10 @@
+man (command) -> f.e. man ls shows the manual for ls command
+
+ls - list content of folder 
+ll - executes ls -la
+echo - creates an output 
+
+who - logged user
+w - logged user with their processes 
+df - filesystem information
+exit - close the shell
