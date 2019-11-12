cd36dd93f909d4392b626a222e2957c5bbfca5e8
Daniel Ullrich <daniel.ullrich@student.fhws.de>
2019-11-12 20:15:17 +0100

    rename userExists and add an info to that script

--- unixAndBash/exercises/userExists.sh
+++ unixAndBash/exercises/userExists.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# http://openbook.rheinwerk-verlag.de/shell_programmierung/shell_006_000.htm#RxxKap00600004004E0C1F017182
+
 if grep "^$1:" /etc/passwd > /dev/null
 then
 	echo "$1 exists"
