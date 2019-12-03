2b969a62f3b7ba1fdfbdfde03e33d4328f1f0a46
Daniel Ullrich <daniel.ullrich@student.fhws.de>
2019-12-03 17:55:58 +0100

    implement everything without ,

--- pointersC/brainfuck/brainfuck.c
+++ pointersC/brainfuck/brainfuck.c
@@ -1,5 +1,40 @@
 #include "brainfuck.h"
+#include <stdio.h>
 
 void interpretBrainfuck(char* input)
 {
+	char workOn[255];
+	for(int i = 0; i < 255; i++)
+		workOn[i]=0;
+	char *ptr = workOn;
+	char *store = NULL;
+	
+	while(*input != '#')
+	{	
+		if(*input == '+')
+			*ptr = *ptr + 1;
+		if(*input == '-')
+			*ptr = *ptr - 1;
+		if(*input == '<')
+			ptr--;
+		if(*input == '>')
+			ptr++;
+		if(*input == '.')
+			printf("%c", *ptr);
+		if(*input == '[' && *ptr != 0)
+		{
+			store = input;
+		}
+		if(*input == '[' && *ptr == 0)
+		{
+			for(;*input != ']'; *input++);
+		}
+		if(store != NULL && *input == ']')
+		{
+			input = store;
+			input--;
+			store = NULL;
+		}
+		input++;			
+	}
 }
